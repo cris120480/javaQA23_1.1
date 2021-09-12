f4c85158d66956998521f6cd8245cca0703e8653
Денис Гришкевич <cris12@mail.ru>
2021-09-12 15:48:18 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balanceNow = 2_000_000_000;
+        int transactionNew = 500_000_000;
+        int totalBalance = balanceNow + transactionNew;
+        System.out.println(totalBalance);
+
+    }
+}
